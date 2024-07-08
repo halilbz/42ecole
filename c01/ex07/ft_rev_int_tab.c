2e152d438b9e51088eb7242750ca64ddc410e1aa
Halil Bozkurt <82759452+halilbz@users.noreply.github.com>
2024-07-08 16:57:27 +0300

    Add files via upload

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,21 +1,29 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibozkurt <ibozkurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 14:35:03 by ibozkurt          #+#    #+#             */
+/*   Updated: 2024/07/08 14:35:05 by ibozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	ilk;
-	int	son;
-	int	gec;
-	
-	ilk  = 0;
-	
+	int		ilk;
+	int		son;
+	int		gec;
+
+	ilk = 0;
 	son = size - 1;
 	while (ilk < size / 2)
 	{
 		gec = tab[son];
-		tab[son] =tab[ilk];
+		tab[son] = tab[ilk];
 		tab[ilk] = gec;
 		ilk++;
 		son--;
 	}
 }
-	
--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,23 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibozkurt <ibozkurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 14:34:19 by ibozkurt          #+#    #+#             */
-/*   Updated: 2024/07/08 14:34:21 by ibozkurt         ###   ########.fr       */
+/*   Created: 2024/07/08 14:35:59 by ibozkurt          #+#    #+#             */
+/*   Updated: 2024/07/08 14:36:01 by ibozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+void	ft_sort_int_tab(int *tab, int size)
 {
-	int		i;
+	int		ilk;
+	int		gec;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (size >= 0)
 	{
-		i++;
+		ilk = 0;
+		while (ilk < size - 1)
+		{
+			if (tab[ilk] > tab[ilk + 1])
+			{
+				gec = tab[ilk];
+				tab[ilk] = tab[ilk + 1];
+				tab[ilk + 1] = gec;
+			}
+			else
+			{
+				ilk++;
+			}
+		}
+		size--;
 	}
-	return (i);
 }

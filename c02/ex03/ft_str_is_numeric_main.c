236f7512cfe63bff37cc644f841c29b7c1f5d982
Halil Bozkurt <82759452+halilbz@users.noreply.github.com>
2024-07-09 22:13:17 +0300

    Add files via upload

---
+++ c02/ex03/ft_str_is_numeric_main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str)
+{
+	int 	i;
+	
+	i = 0;
+	if (str[i] == '\0')
+	{
+		return (1);
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int main (void)
+{
+	char liste[]="1b2356";
+	printf("Result : %d\n",ft_str_is_numeric(liste));
+}

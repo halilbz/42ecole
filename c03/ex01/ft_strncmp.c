2b06428e42084f0ad56dc9fcfa12fe2898c94157
Halil Bozkurt <82759452+halilbz@users.noreply.github.com>
2024-07-20 16:56:18 +0300

    Update ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,23 +1,35 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibozkurt <ibozkurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 11:01:02 by ibozkurt          #+#    #+#             */
+/*   Updated: 2024/07/20 11:01:03 by ibozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
-	
+	unsigned int	i;
+
 	i = 0;
-	if (n == 0)
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		return (0);
-	}
-	
-	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
-	{
+		if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
+
 int main(void)
 {
 	char s1[]="nedenz";
